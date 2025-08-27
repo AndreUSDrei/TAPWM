2b626ccaa09bfd002d4e788e876380468ebd1b15
Andreus <166173807+AndreUSDrei@users.noreply.github.com>
2025-08-27 18:04:41 -0300

    Delete exercicios directory

--- exercicios/callback2.js
+++
@@ -1,8 +0,0 @@
-function exibemensagensNaOrdem(mensagem, callback){
-    console.log(mensagem);
-    callback();
-}
-exibemensagensNaOrdem('Essa é a primeira mensagem exibida na ordem', 
-    function(){
-    console.log('essa é a segunda mensagem exibida na ordem');
-});
